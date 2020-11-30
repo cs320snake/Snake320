11e5c74c238e4abc7e7be918cb06ec094db2257d
AndreaMonzani <andrea.monzani99@gmail.com>
2020-11-30 16:56:18 +0100

    1.3
    registration page (partial) bug @ local:8000/users/registration

--- Snake_project/users/urls.py
+++ Snake_project/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from django.contrib.auth.forms import UserCreationForm
+from . import views
 #from . import views
 app_name = 'users' # to distinguish from other possible apps
 urlpatterns = [
     path('', include('django.contrib.auth.urls')), # adding specifi django authentication (take care lgoin log out)
-
+    path('register/', views.register, name='register')
 ]
--- Snake_project/users/views.py
+++ Snake_project/users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data = request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('game:index')
+    context = {'form':form}
+    return render(request, 'registration/register.html',context)
