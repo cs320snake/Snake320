726333b5e613af3e139d4d8cd41fd7c855cc592f
pangelini9 <pangelini@johncabot.edu>
2020-12-08 17:29:06 +0100

    added (models.Model) to make the Match model inherit from Model

--- Snake_project/game/models.py
+++ Snake_project/game/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Match:
+class Match(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE);
     date = models.DateField();
     score = models.IntegerField();
